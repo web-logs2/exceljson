633d7f2e87776ac631924f9dc15c5a82484916c6
zheng peng <pengzheng@huishoubao.com.cn>
2023-04-26 16:43:57 +0800

    first commit

---
+++ export.go
@@ -0,0 +1,323 @@
+package excelimportexport
+
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+	"strings"
+	"text/template"
+
+	"github.com/pkg/errors"
+
+	"github.com/suifengpiao14/logchan/v2"
+	"github.com/xuri/excelize/v2"
+)
+
+const (
+	FIELDMAP_TYPE_STRING   = "string"
+	FIELDMAP_TYPE_TEMPLATE = "template"
+	TEMPLATE_SEPARATOR     = "{{"
+	EXCEL_CELL_TYPE_FUNC   = "function"
+	EXCEL_CELL_TYPE_STRING = "string"
+)
+
+type FieldMap struct {
+	Type     string
+	CellType string
+	Value    interface{}
+}
+
+var TplFuncMap = template.FuncMap{
+	"fen2yuan": func(fen interface{}) string {
+		var yuan float64
+		intFen, ok := fen.(int)
+		if ok {
+			yuan = float64(intFen) / 100
+			return strconv.FormatFloat(yuan, 'f', 2, 64)
+		}
+		strFen, ok := fen.(string)
+		if ok {
+			intFen, err := strconv.Atoi(strFen)
+			if err == nil {
+				yuan = float64(intFen) / 100
+				return strconv.FormatFloat(yuan, 'f', 2, 64)
+			}
+		}
+		return strFen
+	},
+}
+
+type Export struct{}
+
+//GetRowNumber  获取下一次可以写入的行号
+func (export *Export) GetRowNumber(fd *excelize.File, sheet string) (rowNumber int, err error) {
+	rows, err := fd.Rows(sheet)
+	if err != nil {
+		return
+	}
+	cur := 0
+	for rows.Next() {
+		cur++
+	}
+	rowNumber = cur + 1
+	return
+}
+
+// 移除一行
+func (export *Export) RemoveRow(fd *excelize.File, sheet string, row int) (err error) {
+	err = fd.RemoveRow(sheet, row)
+	return
+}
+
+func (export *Export) GetFieldMap(fd *excelize.File, sheet string) (fieldMap map[int]*FieldMap, err error) {
+	fieldMap = make(map[int]*FieldMap)
+	rows, err := fd.GetRows(sheet)
+	if err != nil {
+		return
+	}
+	if len(rows) < 2 {
+		err = errors.New("excel file must have more than tow row")
+		return
+	}
+	row := rows[0]
+	for colIndex, colCell := range row {
+		index := colIndex + 1 // excel索引从1开始
+		if strings.Contains(colCell, TEMPLATE_SEPARATOR) {
+			tplName := fmt.Sprintf("field_%d_tpl", index)
+			tpl, err := template.New(tplName).Funcs(TplFuncMap).Parse(colCell)
+			if err != nil {
+				return nil, err
+			}
+			cellType := EXCEL_CELL_TYPE_STRING
+			if colCell[0:1] == "=" {
+				cellType = EXCEL_CELL_TYPE_FUNC
+			}
+
+			fieldMap[index] = &FieldMap{
+				Type:     FIELDMAP_TYPE_TEMPLATE,
+				CellType: cellType,
+				Value:    tpl,
+			}
+		} else {
+			fieldMap[index] = &FieldMap{
+				Type:     FIELDMAP_TYPE_STRING,
+				CellType: EXCEL_CELL_TYPE_STRING,
+				Value:    colCell,
+			}
+		}
+	}
+	return
+}
+
+//WriteDataToFile  写入数据
+func (export *Export) WriteDataToFile(fd *excelize.File, sheet string, rowNumber int, fieldMap map[int]*FieldMap, data []map[string]interface{}) (nextRowNumber int, err error) {
+	streamWriter, err := export.GetStreamWriter(fd, sheet)
+	if err != nil {
+		return
+	}
+	defer func() {
+		err = streamWriter.Flush()
+		if err != nil {
+			return
+		}
+	}()
+
+	minColIndex := -1
+	// 找到每行开始写入的列序号
+	for colIndex := range fieldMap {
+		if minColIndex == -1 {
+			minColIndex = colIndex
+			continue
+		}
+		if colIndex < minColIndex {
+			minColIndex = colIndex // 找到最小的列
+		}
+	}
+
+	colLen := len(fieldMap)
+	for _, record := range data {
+		// 组装一行数据
+		row := make([]interface{}, colLen)
+		for colIndex, field := range fieldMap {
+			k := colIndex - 1
+			val, err := export.GetValue(field, record)
+			if err != nil {
+				return 0, err
+			}
+			row[k] = val
+		}
+
+		// 获取当前行开始写入单元地址
+		cell, err := excelize.CoordinatesToCellName(minColIndex, rowNumber)
+		if err != nil {
+			return 0, err
+		}
+		//写入一行数据
+		err = streamWriter.SetRow(cell, row)
+		if err != nil {
+			return 0, err
+		}
+		rowNumber++ // 增加行号
+	}
+	nextRowNumber = rowNumber
+	return
+}
+
+func (export *Export) GetValue(field *FieldMap, data map[string]interface{}) (value interface{}, err error) {
+	if field.Type == FIELDMAP_TYPE_STRING {
+		fieldKey, ok := field.Value.(string)
+		if !ok {
+			err = fmt.Errorf("%v can not convert to string", field.Value)
+			return
+		}
+		value, ok := data[fieldKey]
+		if ok {
+			return value, nil
+		}
+		return fieldKey, nil // 对于数据中不存在的列，使用模板中列标题填充，确保数据能对应上
+	}
+	// 处理go模板
+	if field.Type == FIELDMAP_TYPE_TEMPLATE {
+		tpl, ok := field.Value.(*template.Template)
+		if !ok {
+			err = fmt.Errorf("%v can not convert to text/template", field.Value)
+			return
+		}
+		var rw bytes.Buffer
+		if err := tpl.Execute(&rw, data); err != nil {
+			return nil, err
+		}
+		str := rw.String()
+		return str, nil
+	}
+	return field.Value, nil
+}
+
+//Write2streamWriter 向写入流中写入数据
+func (export *Export) Write2streamWriter(streamWriter *excelize.StreamWriter, rowNumber int, fieldMap map[int]*FieldMap, data []map[string]interface{}) (nextRowNumber int, err error) {
+	minColIndex := 999999999 // 默认从最右边写入
+	// 找到每行开始写入的列序号
+	for colIndex := range fieldMap {
+		if colIndex < minColIndex {
+			minColIndex = colIndex // 找到最小的列
+		}
+	}
+
+	colLen := len(fieldMap)
+	for _, record := range data {
+		// 组装一行数据
+		row := make([]interface{}, colLen)
+		for colIndex, field := range fieldMap {
+			k := colIndex - 1
+			val, err := export.GetValue(field, record)
+			if err != nil {
+				return 0, err
+			}
+			row[k] = val
+		}
+
+		// 获取当前行开始写入单元地址
+		cell, err := excelize.CoordinatesToCellName(minColIndex, rowNumber)
+		if err != nil {
+			return 0, err
+		}
+		//写入一行数据
+		err = streamWriter.SetRow(cell, row)
+		if err != nil {
+			return 0, err
+		}
+		rowNumber++ // 增加行号
+	}
+	nextRowNumber = rowNumber
+	return
+}
+
+//GetStreamWriter 打开文件流，将已有的数据填写到流内，返回写入流
+func (export *Export) GetStreamWriter(fd *excelize.File, sheet string) (streamWriter *excelize.StreamWriter, err error) {
+	streamWriter, err = fd.NewStreamWriter(sheet)
+	if err != nil {
+		return
+	}
+
+	rows, err := fd.GetRows(sheet) //获取行内容
+	if err != nil {
+		return
+	}
+	//将源文件内容先写入excel
+	for rowindex, oldRow := range rows {
+		colLen := len(oldRow)
+		newRow := make([]interface{}, colLen)
+		for colIndex := 0; colIndex < colLen; colIndex++ {
+			if oldRow == nil {
+				newRow[colIndex] = nil
+			} else {
+				newRow[colIndex] = oldRow[colIndex]
+			}
+		}
+		beginCell, _ := excelize.CoordinatesToCellName(1, rowindex+1)
+		err = streamWriter.SetRow(beginCell, newRow)
+		if err != nil {
+			return
+		}
+	}
+	return
+}
+
+type DataWrap struct {
+	Data      []map[string]interface{}
+	RowNumber int
+}
+
+const (
+	LOG_INFO_READ_CHAN_DATA = "export_read_chan_data"
+)
+
+type LogInfoReadChanData struct {
+	logchan.EmptyLogInfo
+	err error
+}
+
+func (l *LogInfoReadChanData) GetName() (name string) {
+	name = LOG_INFO_READ_CHAN_DATA
+	return name
+}
+
+func (l *LogInfoReadChanData) Error() (err error) {
+	err = l.err
+	return err
+}
+
+//ReadChanData 接收chan中的数据,写入文件,完成后输出完成信号，并退出
+func (export *Export) ReadChanData(dataChan chan *DataWrap, fd *excelize.File, sheet string, fieldMap map[int]*FieldMap) (finishSignal chan struct{}, err error) {
+	streamWriter, err := export.GetStreamWriter(fd, sheet)
+	if err != nil {
+		return nil, err
+	}
+	finishSignal = make(chan struct{}, 1)
+	go func() {
+		logInfo := LogInfoReadChanData{}
+		defer func() {
+			finishSignal <- struct{}{} // 发送完成信号
+		}()
+		for dataWrap := range dataChan { //  阻塞读取数据写入文件
+			_, err = export.Write2streamWriter(streamWriter, dataWrap.RowNumber, fieldMap, dataWrap.Data)
+			if err != nil {
+				logInfo.err = err
+				return
+			}
+		}
+
+		// 保存文件
+		err = streamWriter.Flush()
+		if err != nil {
+			logInfo.err = err
+			return
+		}
+		err = fd.Save()
+		if err != nil {
+			logInfo.err = err
+			return
+		}
+	}()
+	return finishSignal, nil
+}
---
+++ parse.go
@@ -0,0 +1,108 @@
+package excelimportexport
+
+import (
+	"github.com/xuri/excelize/v2"
+)
+
+type parse struct {
+}
+
+// NewParseService 实例化 excel服务
+func NewParseService() *parse {
+	return &parse{}
+}
+
+// Read 读取excel 表中所有数据
+func (instance *parse) Read(f *excelize.File, sheet string, fieldMap map[string]string, rowIndex int, isUnmergeCell bool) ([]map[string]string, error) {
+	if isUnmergeCell {
+		err := instance.UnmergeCell(f, sheet)
+		if err != nil {
+			return nil, err
+		}
+	}
+	// 获取 Sheet 上所有单元格
+	rows, err := f.GetRows(sheet)
+	if err != nil {
+		return nil, err
+	}
+	output := make([]map[string]string, 0)
+
+	for index, row := range rows {
+		if index < rowIndex-1 { // 从指定行开始读取
+			continue
+		}
+		record := make(map[string]string, 0)
+		for colIndex, colCell := range row {
+			colName, err := excelize.ColumnNumberToName(colIndex + 1)
+			if err != nil {
+				return nil, err
+			}
+			if fieldMap != nil { // 如果定制了列名和字段映射关系，替换字段映射
+				field, ok := fieldMap[colName]
+				if ok {
+					record[field] = colCell
+				}
+			} else {
+				record[colName] = colCell
+			}
+		}
+		output = append(output, record)
+	}
+	return output, nil
+}
+
+//UnmergeCell 将合并单元格展开，值填充到每个展开的单元内
+func (instance *parse) UnmergeCell(f *excelize.File, sheet string) (err error) {
+	mergeCells, err := f.GetMergeCells(sheet)
+	if err != nil {
+		return err
+	}
+	if len(mergeCells) == 0 {
+		return nil
+	}
+	for _, mergeCell := range mergeCells {
+		startAxis := mergeCell.GetStartAxis()
+		endAxis := mergeCell.GetEndAxis()
+		value := mergeCell.GetCellValue()
+		err = f.UnmergeCell(sheet, startAxis, endAxis)
+		if err != nil {
+			return err
+		}
+		cells, err := expandCellRegion(startAxis, endAxis)
+		if err != nil {
+			return err
+		}
+		for _, cell := range cells {
+			err = f.SetCellValue(sheet, cell, value)
+			if err != nil {
+				return err
+			}
+		}
+
+	}
+	return nil
+}
+
+// expandCellRegion 将合并区域展开成单个单元地址集合
+func expandCellRegion(startAxis string, endAxis string) (cells []string, err error) {
+	startColumn, startRow, err := excelize.CellNameToCoordinates(startAxis)
+	if err != nil {
+		return nil, err
+	}
+	endColumn, endRow, err := excelize.CellNameToCoordinates(endAxis)
+	if err != nil {
+		return nil, err
+	}
+
+	cells = make([]string, 0)
+	for row := startRow; row <= endRow; row++ {
+		for column := startColumn; column <= endColumn; column++ {
+			cell, err := excelize.CoordinatesToCellName(column, row)
+			if err != nil {
+				return nil, err
+			}
+			cells = append(cells, cell)
+		}
+	}
+	return cells, nil
+}
